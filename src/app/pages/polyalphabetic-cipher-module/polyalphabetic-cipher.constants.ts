244c91fa10731801b4a92a7e3d798a8fca397f64
frantisek.ff <frantisek.ff@gmail.com>
2020-04-18 14:14:49 +0200

    PolyAlphab. c. to new design

--- src/app/pages/polyalphabetic-cipher-module/polyalphabetic-cipher.constants.ts
+++ src/app/pages/polyalphabetic-cipher-module/polyalphabetic-cipher.constants.ts
@@ -1,16 +1,49 @@
-export const NAME_CIPHER = 'Vigenere Cipher';
-export const TYPE_CIPHER = 'Polyalphabetic';
+export const NAME_CIPHER = "Vigenere Cipher";
+export const TYPE_CIPHER = "Polyalphabetic";
 
-export const COLUMNS_REFFREQ_LANGUAGE: string[] = ['name', 'value'];
+export const COLUMNS_REFFREQ_LANGUAGE: string[] = ["name", "value"];
 
 export const MESSAGE =
-    'theenglishwikipediawasthefirstwikipediaeditionandhasremainedthelargestithaspioneeredmanyi' +
-    'deasasconventionspoliciesorfeatureswhichwerelateradoptedbywikipediaeditionsinsomeoftheother' +
-    'languagestheseideasincludefeaturedarticlestheneutralpointofviewpolicynavigationtemplatesthesor' +
-    'tingofshortstubarticlesintosubcategoriesdisputeresolutionmechanismssuchasmediationandarbitration' +
-    'andweeklycollaborationstheenglishwikipediahasadoptedfeaturesfromwikipediasinotherlanguagesthesefeatures' +
-    'includeverifiedrevisionsfromthegermanwikipediadewikiandtownpopulationlookuptemplatesfromthedutchwikipedianlwikial' +
-    'thoughtheenglishwikipediastoresimagesandaudiofilesaswellastextfilesmanyoftheimageshavebeenmovedtowikimediacommonswiththe' +
-    'samenameaspassedthroughfileshowevertheenglishwikipediaalsohasfairuseimagesandaudiovideofileswithcopyrightrestrictionsmostof' +
-    'whicharenotallowedoncommonsmanyofthemostactiveparticipantsinthewikimediafoundationandthedevelopersofthemediawikisoftwarethat' +
-    'powerswikipediaareenglishusers';
+  "theenglishwikipediawasthefirstwikipediaeditionandhasremainedthelargestithaspioneeredmanyi" +
+  "deasasconventionspoliciesorfeatureswhichwerelateradoptedbywikipediaeditionsinsomeoftheother" +
+  "languagestheseideasincludefeaturedarticlestheneutralpointofviewpolicynavigationtemplatesthesor" +
+  "tingofshortstubarticlesintosubcategoriesdisputeresolutionmechanismssuchasmediationandarbitration" +
+  "andweeklycollaborationstheenglishwikipediahasadoptedfeaturesfromwikipediasinotherlanguagesthesefeatures" +
+  "includeverifiedrevisionsfromthegermanwikipediadewikiandtownpopulationlookuptemplatesfromthedutchwikipedianlwikial" +
+  "thoughtheenglishwikipediastoresimagesandaudiofilesaswellastextfilesmanyoftheimageshavebeenmovedtowikimediacommonswiththe" +
+  "samenameaspassedthroughfileshowevertheenglishwikipediaalsohasfairuseimagesandaudiovideofileswithcopyrightrestrictionsmostof" +
+  "whicharenotallowedoncommonsmanyofthemostactiveparticipantsinthewikimediafoundationandthedevelopersofthemediawikisoftwarethat" +
+  "powerswikipediaareenglishusers";
+
+export const TOP_GAP = 110;
+export const SIDE_MENU = [
+  {
+    title: "Set inputs",
+    active: true,
+    id: "inputs",
+    bottomPosition: 0,
+    topPosition: 0,
+  },
+  {
+    title: "Alghorithm to find key",
+    active: true,
+    id: "alghorithm",
+    bottomPosition: 0,
+    topPosition: 0,
+  },
+  {
+    title: "Show IC for different length of key",
+    active: true,
+    id: "findkey",
+    bottomPosition: 0,
+    topPosition: 0,
+  },
+  {
+    title: "Compute all combinations for key",
+    active: true,
+    id: "allcombinations",
+    bottomPosition: 0,
+    topPosition: 0,
+  },
+
+];
